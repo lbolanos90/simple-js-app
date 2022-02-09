35bdf03cbc78a2bbc8c17697b5c053ec75cb7c1f
lbolanos90 <lcbolanos07@gmail.com>
2022-02-09 17:41:13 -0300

    Update scripts.js
    Added a shorter way to print pokemon's name and height.

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,12 @@ let pokemonList = [
         type: 'water'
     }
 ]
+//Below is just one example of how I can have the pokemon's names print.
+// for ( let i = 0; i < pokemonList.length; i++) {
+//     document.write('<p>' + pokemonList[i].name + (', height:')+ pokemonList[i].height + '</p>'); 
+// }
 
-for ( let i = 0; i < pokemonList.length; i++) {
-    document.write('<p>' + pokemonList[i].name + (', height:')+ pokemonList[i].height + '</p>'); 
+// Here is a shorter example of how to pring pokemon's name and height. 
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})<br>`)
 }
