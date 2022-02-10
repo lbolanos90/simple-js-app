ad8cbc12bd4fd9d5f65bd670bc6a195420442493
lbolanos90 <lcbolanos07@gmail.com>
2022-02-10 10:10:36 -0300

    Update scripts.js
    Simplified loop code for pokemon. Added conditional but not working. Added comments.

--- js/scripts.js
+++ js/scripts.js
@@ -16,18 +16,23 @@ let pokemonList = [
         type: 'water'
     }
 ]
-//Below is just one example of how I can have the pokemon's names print.
-// for ( let i = 0; i < pokemonList.length; i++) {
-//     document.write('<p>' + pokemonList[i].name + (', height:')+ pokemonList[i].height + '</p>'); 
-// }
 
-let output = "";
-// Here is a shorter example of how to pring pokemon's name and height. 
-for (let i = 0; i < pokemonList.length; i++) {
-    output = `${pokemonList[i].name} (height: ${pokemonList[i].height})`;
-    //Adding comment "Wow, that's big!" for largest pokemon.
-    if(pokemonList[i].height > 4) {
-        output = `${output} - Wow, that's big!`;
-    }
-    document.write(`${output}<br>`);
+let i = 0;
+//This prints the pokemon's name and height.
+for ( let i = 0; i < pokemonList.length; i++) {
+    document.write('<p>' + pokemonList[i].name + (', height:')+ pokemonList[i].height + '</p>'); 
 }
+/* I tried to add the parenthesis around the "height" text. But it ended up doing this instead: Charmander (height:) 5
+
+This is what I tried to do: 
+('<p>' + pokemonList[i].name + (',( height: )')+ pokemonList[i].height + '</p>'); 
+ 
+But it didn't work.  
+*/
+
+
+//I still can't get this to print properly. Supposed to print "Wow that big for Charmander"
+    if (pokemonList[i].height > 4) {
+        document.write;('<p>' + pokemonList[i].name + (', height:')+ pokemonList[i].height + (' - Wow, that\'s big!') + '</p>')
+    }
+    
