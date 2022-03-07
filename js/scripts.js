d51b2d73bfc860cccad1b7482806db1b2a3446bb
lbolanos90 <lcbolanos07@gmail.com>
2022-03-07 11:58:53 -0300

    JS cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -89,6 +89,7 @@ return {
 pokemonRepository.loadList().then(function() {
   pokemonRepository.getAll().forEach(function(pokemon){
     pokemonRepository.addListItem(pokemon); 
-  }); 
-}); 
-
+  })
+});
+  
+  
